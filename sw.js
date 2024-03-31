7b1efb10c247479f2abf3c9fe7c1ac6dc8756d3d
Germán Gómez <germangomezc@gmail.com>
2024-03-31 00:15:28 -0600

    modificaciones de iphone x+

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 //imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v2';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v3';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
-const APP_SHELL =  [
+const APP_SHELL =  [
     // '/',
     'index.html',
     'css/style.css',
@@ -43,6 +43,9 @@ self.addEventListener('activate', e => {
     const respuesta = caches.keys().then(keys => {
         keys.forEach(k => {
             if( k !== STATIC_CACHE && k.includes('static')){
+                return caches.delete(k);
+            }
+            if( k !== DYNAMIC_CACHE && k.includes('dynamic')){
                 return caches.delete(k);
             }
         });
